19aeb903c24ed3dcb932488ab64f5f23e08ff511
davWK <davwoglo@gmail.com>
2021-11-21 13:11:46 +0000

    Make the script more human readable

--- By_me/disk_check.py
+++ By_me/disk_check.py
@@ -7,21 +7,18 @@ import psutil #module in which there is cpu_percent function
 def check_disk(disk):
     du = shutil.disk_usage('/') #stock disk usage info (total, free, used)
     free_space = (du.free/du.total)*100
-    return free_space > 20 #good if the free space is grater than 20%
+    free_space = float("{:.2f}".format(free_space))
+    return free_space #(< 20), #good if the free space is grater than 20%
 
 # cpu utilization check function
 def check_cpu():
-    cpu_usage = psutil.cpu_percentage(1)
-    return cpu_usage < 75 #good if the cpu utlzation is less than 75%
+    cpu_usage = psutil.cpu_percent(1)
+    cpu_usage = float("{:.2f}".format(cpu_usage))
+    return cpu_usage # (> 75) #good if the cpu utlzation is less than 75%
+
 
 #main body of the script
-if not check_disk("/") or not check_cpu():
-    print("ounhmmm we got a problem :(")
+if  check_disk("/") < 20 or check_cpu() > 75:
+    print("We got a problem :( \nonly {} % disk space remain and CPU is used at {} %".format(check_disk("/"), check_cpu()))
 else:
-    prin("ouff !!! Everything is OK :)")
-
-
-
-
-du_read = "{:.2f}".format(du_read)
-print("{}% disk space is used".format(du_read))
+    print("Everything is OK :)")
